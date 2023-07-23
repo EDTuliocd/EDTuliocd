4126e38c355125ae50f60807ae1c6cba194f94c9
TULIO CD <138325577+EDTuliocd@users.noreply.github.com>
2023-07-23 12:56:49 -0500

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -3,7 +3,7 @@
 
 echo "Hello Wolrd- Hello Navigators"
 echo "El nombre de tu script es $0"
-Sorteo 1523 - 20/07/2023
+variableSorteo 1523 - 20/07/2023
 Pozo Acumulado: 28 5 23 9 18 32
 Chau Chamba: 10 40 8 16 2 1
 Sorteo 1522 - 18/07/2023
@@ -19,23 +19,23 @@ Sorteo 1519 - 11/07/2023
 Pozo Acumulado: 38 31 10 6 13 25
 Chau Chamba: 36 18 6 25 7 4
 Sorteo 1518 - 08/07/2023
-Pozo Acumulado: 30 20 7 40 5 11
-Chau Chamba: 13 40 39 36 10 26
-Sorteo 1517 - 06/07/2023
-Pozo Acumulado: 24 3 34 7 15 14
-Chau Chamba: 17 2 36 25 6 13
-Sorteo 1516 - 04/07/2023
-Pozo Acumulado: 30 28 1 31 20 6
-Chau Chamba: 17 37 20 32 11 24
-variable1="Sorteo 1515 01/07/2023"
-PozoAcumulado1=" 1 28 13 5 2 17"
-ChauChamba1="2 39 9 32 6 28"
+Pozo Acumulado6=" 30 20 7 40 5 11#
+Chau Chamba6=" 13 40 39 36 10 26"
+variable5="Sorteo 1517 - 06/07/2023"
+Pozo Acumulado5=" 24 3 34 7 15 14"
+Chau Chamba5=" 17 2 36 25 6 13"
+variable4="Sorteo 1516 - 04/07/2023"
+Pozo Acumulado4=" 30 28 1 31 20 6"
+ChauChamba4=" 17 37 20 32 11 24"
+variable3="Sorteo 1515 01/07/2023"
+PozoAcumulado3=" 1 28 13 5 2 17"
+ChauChamba3="2 39 9 32 6 28"
 variable2="Sorteo 1514 29/06/2023"
 PozoAcumulapdo2=" 23 6 30 18 27 11"
 ChauChamba2="9 20 23 28 6 21"
-variable3="Sorteo 1513 27/06/2023"
-PozoAcumulado3=" 29 13 2 28 33 7"
-ChauChamba3="4 7 24 8 34 35"
+variable1="Sorteo 1513 27/06/2023"
+PozoAcumulado1=" 29 13 2 28 33 7"
+ChauChamba1="4 7 24 8 34 35"
 echo $variable1
 echo $ChauChamba1
 echo $PozoAcumulado1

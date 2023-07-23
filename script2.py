c59881f45cb7ecd68b53fbb2e7f7f9bc893daa1b
TULIO CD <138325577+EDTuliocd@users.noreply.github.com>
2023-07-23 13:28:38 -0500

    mejorando con python

---
+++ script2.py
@@ -0,0 +1,65 @@
+#¡  /bin/bash
+#este es un recopilador de datos
+print("Hola Navegantes")
+print("Hello World - Hellow navigators")
+print("Bienvenidos a mi Universo, donde todo es posible para el que cree")
+print(" ¿Y tu, crees que es posible?")
+conviccion = input()
+print("El nombre de tu script es $0")
+Sorteo10(" 1523 - 20/07/2023")
+Pozo Acumulado10(" 28 5 23 9 18 32")
+Chau Chamba10(" 10 40 8 16 2 1")
+variable10="Sorteo 1522 - 18/07/2023" 
+Pozo Acumulado10=" 23 33 31 22 40 10"
+Chau Chamba10=" 6 5 27 33 9 3"
+variable9="Sorteo 1521 - 15/07/2023"
+Pozo Acumulado9=" 23 1 10 21 5 33"
+Chau Chamba9=" 14 22 3 38 2 32"
+variable8="Sorteo 1520 - 13/07/2023"
+Pozo Acumulado8=" 32 36 34 25 23 21"
+Chau Chamba8=" 33 36 39 32 23 2"
+variable7="Sorteo 1519 - 11/07/2023"
+Pozo Acumulado7=" 38 31 10 6 13 25"
+Chau Chamba7=" 36 18 6 25 7 4"
+variableSorteo6=" 1518 - 08/07/2023"
+Pozo Acumulado6=" 30 20 7 40 5 11#
+Chau Chamba6=" 13 40 39 36 10 26"
+variable5="Sorteo 1517 - 06/07/2023"
+Pozo Acumulado5=" 24 3 34 7 15 14"
+Chau Chamba5=" 17 2 36 25 6 13"
+variable4="Sorteo 1516 - 04/07/2023"
+Pozo Acumulado4=" 30 28 1 31 20 6"
+ChauChamba4=" 17 37 20 32 11 24"
+variable3="Sorteo 1515 01/07/2023"
+PozoAcumulado3=" 1 28 13 5 2 17"
+ChauChamba3="2 39 9 32 6 28"
+variable2="Sorteo 1514 29/06/2023"
+PozoAcumulapdo2=" 23 6 30 18 27 11"
+ChauChamba2="9 20 23 28 6 21"
+variable1="Sorteo 1513 27/06/2023"
+PozoAcumulado1=" 29 13 2 28 33 7"
+ChauChamba1="4 7 24 8 34 35"
+echo $variable1
+echo $ChauChamba1
+echo $PozoAcumulado1
+echo $variable2
+echo $ChauChamba2
+echo $variable3
+echo $ChauChamba3
+echo $PozoAcumulado3
+echo "El numero de parametros es $#"
+echo "todos los parametros son  = $*
+echo $$
+echo "Tiempo inicio script $SECONDS"
+echo "Tiempo inicio script $MINUTES"
+echo "Lines totales $LINENO"
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo

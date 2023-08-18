f316cf2b6373a3fc374fd3fbdd12f93a246f6ab5
TULIO CD <138325577+EDTuliocd@users.noreply.github.com>
2023-08-17 20:49:02 -0500

    Add files via upload

---
+++ NumerosAGD.py
@@ -0,0 +1,55 @@
+#¡  /python/NumerosAk.py
+#este es un recopilador de datos
+print ("Hola Navegantes")
+print ("Hello World - Hellow navigators")
+print ("Bienvenidos a mi Universo, donde todo es posible para el qe ctee")
+print (" ¿Y tu, crees que es posible?")
+conviccion = input()
+#print("El nombre de tu script es $0")
+print("Sorteo 3563 - 31/07/2023")
+Bolillas = [22, 28, 5, 29, 1]
+print ("Sorteo 3562 - 30/07/2023")
+Bolillas = [24, 15, 29, 27, 12]
+print ("Sorteo 3561 - 29/07/2023")
+Bolillas = [15, 31, 17, 28, 3]
+print ("Sorteo 3560 - 28/07/2023")
+Bolillas = [20, 22, 34, 17, 6]
+print ("Sorteo 3559 - 27/07/2023")
+Bolillas = [27, 29, 23, 15, 21]
+print ("Sorteo 3558 - 26/07/2023")
+Bolillas = [7, 1, 9, 33, 24]
+print ("Sorteo 3557 - 25/07/2023")
+Bolillas = [1, 24, 31, 10, 27]
+print ("Sorteo 3556 - 24/07/2023")
+Bolillas = [32, 5, 31, 25, 1]
+print ("Sorteo 3555 - 23/07/2023")
+Bolillas = [22, 28, 17, 29, 23]
+print ("Sorteo 3554 - 22/07/2023")
+Bolillas = [34, 11, 29, 17, 26]
+print ("Sorteo 3553 - 21/07/2023")
+Bolillas = [21, 4, 16, 10, 35]
+print ("Sorteo 3552 - 20/07/2023")
+Bolillas = [9, 33, 32, 6, 3]
+print ("Sorteo 3551 - 19/07/2023")
+Bolillas = [21, 10, 14, 32, 35]
+print ("Sorteo 3550 - 18/07/2023")
+Bolillas = [13, 11, 1, 34, 7]
+
+
+for i in range (len(numeros)):
+     for j in range (len(numeros)):
+         if i != j:
+              if numeros(i) == numeros[j] and numeros[i] not in repetidos:
+                  repetidos. append(numeros[i])
+
+
+print (repetidos)
+
+
+# 1 Busquemos números repetidos entre 0 y 40
+mi_lista = []
+import random 
+import time
+
+
+

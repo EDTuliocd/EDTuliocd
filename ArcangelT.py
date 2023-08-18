e16c140dbc3836f421e2a4159764807a1c30e33a
TULIO CD <138325577+EDTuliocd@users.noreply.github.com>
2023-08-17 20:42:04 -0500

    Add files via upload

--- ArcangelT.py
+++ ArcangelT.py
@@ -1,26 +1,25 @@
-2print("Hola Navegantes")
-print("Bienvenidos a mi Universo, donde todo es posible para el que cree")
-print(" ¿Y tu, crees que es posible?")
+#python/ArcangelT
+print ("Hello Navegantes")
+print ("Bienvenidos a mi Universo, donde todo es posible para el que cree")
+print (" ¿Y tu, crees que es posible?")
 conviccion = input()
  
-print(" Haz de saber que tu maximo pensamiento es la respuesta del deseo de tu Corazón, son cosas que se hacen realidad con la acción de la fuerza y la voluntad")
-print("¿como te llamas?")
+print (" Haz de saber que tu maximo pensamiento es la respuesta del deseo de tu Corazón, ")
+print ("¿como te llamas?")
 nombre = input()
-print( nombre + " ¿estas convencido de tu respuesta? ")
+print ( nombre + " ¿estas convencido de tu respuesta? ")
 conviccion = input()
-print("Hola " + nombre + ", Soy T el Arcangel de la Riqueza" )
-print(" ¿Quieres saber que numeros cambiaran tu vida? : ")
+print ("Hola " + nombre + ", Soy T el Arcangel de la Riqueza" )
+print (" ¿Dedeas saber que numeros cambiaran tu vida? : ")
 conviccion = input()
 
-print("concatenación:")
-mensaje = " Hola"
-espacio = " "
-nombre = "Arcangel T, Dame mi Bendicion"
-print(mensaje + espacio + nombre)
+nombre = "Arcangel T, Dame la Bendicion que mi Padre Celestial dueño del Oro y la Plata te ha dado para mi"
+print ( nombre)
+print ("...Deseo concedido")
 
 import random
 
-#l.Let's generate 7 values between 1 and 40. Store valued on a list.random
+#l.Let's generate 6 values between 1 and 48. Store valued on a list.random
 
 my_list = []
 
@@ -30,17 +29,17 @@ for _ in range (6):
   
 print(my_list)
 
-# 2 - let's generate 3 odd values between 0 and 48
+# 2 - let's generate 3 odd values between 1 and 48
 my_list = []
 
-# for _ in range (3):
-#   num = random.randrange(0, 48, 1)
-#   my_list.aopend(num)
+for _ in range (3):
+  num = random.randrange(0, 48, 1)
+  my_list.append(num)
     
-# print(my_list)
+print(my_list)
 
-# 3 - Generate 6 random numbers multiple of 5
-for _ in range(6):
+# 3 - Generate 3 random numbers multiple of 5
+for _ in range(3):
   rand_num = random.randint(1, 9)*5
   print(rand_num)
   

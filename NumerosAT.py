15329eb0827f72a35342bf8567f0b77f0ee61ab1
TULIO CD <138325577+EDTuliocd@users.noreply.github.com>
2023-08-23 20:37:24 -0500

    Add files via upload

--- NumerosAT.py
+++ NumerosAT.py
@@ -10,38 +10,39 @@ conviccion = input ()
 
 '''
 Buscar numeros repetidos de numeros
-Buscar numeros repetidos de numerosBoliyapa
-Buscar nuneros repetidos de numerosSíoSí
+Buscar nuneros repetidos de numerosboliyapa
+Buscar nuneros repetidos de numerosSioSi
 '''
 
-print ("Sorteo 1010 - 09/08/2023")
-print ("Sorteo 1009 - 06/08/2023")
-print ("Sorteo 1008 - 02/08/2023")
-print ("Sorteo 1007 - 30/07/2023")
-print ("Sorteo 1006 - 26/07/2023")
-print ("Sorteo 1005 - 23/07/2023")
-print ("Sorteo 1004 - 19/07/2023")
-print ("Sorteo 1003 - 16/07/2023")
-print ("Sorteo 1002 - 12/07/2023")
-print ("Sorteo 1001 - 09/07/2023")
-print ("Sorteo 1000 - 05/07/2023")
-print ("Sorteo 999 - 02/07/2023")
-print ("Sorteo 998 - 28/06/2023")
-print ("Sorteo 997 - 25/06/2023")
-print ("Sorteo 996 - 21/06/2023")
-print ("Sorteo 995 - 18/06/2023")
-print ("Sorteo 994 - 14/06/2023")
+#print ("Sorteo 1013 - 20/08/2023")
+#print ("Sorteo 1012 - 16/08/2023")
+#print ("Sorteo 1011 - 13/08/2023")
+#print ("Sorteo 1010 - 09/08/2023")
+#print ("Sorteo 1009 - 06/08/2023")
+#print ("Sorteo 1008 - 02/08/2023")
+#print ("Sorteo 1007 - 30/07/2023")
+#print ("Sorteo 1006 - 26/07/2023")
+#print ("Sorteo 1005 - 23/07/2023")
+#print ("Sorteo 1004 - 19/07/2023")
+#print ("Sorteo 1003 - 16/07/2023")
+#print ("Sorteo 1002 - 12/07/2023")
+#print ("Sorteo 1001 - 09/07/2023")
+#print ("Sorteo 1000 - 05/07/2023")
+#print ("Sorteo 999 - 02/07/2023")
+#print ("Sorteo 998 - 28/06/2023")
+#print ("Sorteo 997 - 25/06/2023")
+#print ("Sorteo 996 - 21/06/2023")
+#print ("Sorteo 995 - 18/06/2023")
+#print ("Sorteo 994 - 14/06/2023")
 #
-#
-numeros = [32, 42, 23, 2, 24, 27, 47, 25, 39, 35, 27, 6, 42, 38, 5, 33, 16, 12, 40, 21, 39, 13, 17, 26, 24, 15, 39, 15, 38, 31, 9, 26, 43, 6, 38, 35, 23, 9, 31, 32, 48, 24, 44, 33, 25, 1, 13, 20, 42, 27, 23, 26, 38, 29, 21, 6, 11, 34, 28, 11, 1, 24, 5, 12, 43, 20, 10, 13, 22, 35, 43, 34, 14, 7, 38, 19, 26, 23, 25, 2, 11, 26, 1, 38, 13, 42, 30, 29, 13, 27, 21, 25, 11, 27, 2, 22, 9, 18, 41]
+numeros = [43, 25, 27, 34, 6, 7, 37, 19, 13, 26, 45, 2, 42, 19, 25, 38, 12, 37, 32, 42, 23, 2, 24, 27, 47, 25, 39, 35, 27, 6, 42, 38, 5, 33, 16, 12, 40, 21, 39, 13, 17, 26, 24, 15, 39, 15, 38, 31, 9, 26, 43, 6, 38, 35, 23, 9, 31, 32, 48, 24, 44, 33, 25, 1, 13, 20, 42, 27, 23, 26, 38, 29, 21, 6, 11, 34, 28, 11, 1, 24, 5, 12, 43, 20, 10, 13, 22, 35, 43, 34, 14, 7, 38, 19, 26, 23, 25, 2, 11, 26, 1, 38, 13, 42, 30, 29, 13, 27, 21, 25, 11, 27, 2, 22, 9, 18, 41]
 
-numerosBoliyapa = [25, 9, 4, 30, 22, 34, 10, 2, 41, 45, 30, 31, 20, 3, 43, 40, 14]
+numerosBoliyapa = [31, 32, 35, 38, 25, 9, 4, 30, 22, 34, 10, 2, 41, 45, 30, 31, 20, 3, 43, 40, 14]
 
-numerosSíoSí = [45, 34, 9, 34, 30, 26, 32, 11, 29, 8, 13, 48, 7, 29, 30, 33, 16, 4, 47, 17, 10, 24, 10, 15, 44, 5, 42, 39, 44, 9, 32, 7, 19, 21, 40, 37, 14, 39, 29, 13, 26] 
+numerosSíoSí = [ 9, 35, 25, 33, 38, 1, 27, 45, 34, 9, 34, 30, 26, 32, 11, 29, 8, 13, 48, 7, 29, 30, 33, 16, 4, 47, 17, 10, 24, 10, 15, 44, 5, 42, 39, 44, 9, 32, 7, 19, 21, 40, 37, 14, 39, 29, 13, 26] 
 
 repetidos = []
 archivo = []
-
 
 for i in range(len (numeros)) :
     for j in range(len(numeros)) :
@@ -69,28 +70,25 @@ print (numerosBoliyapa)
 print (numerosSíoSí)
 #
 #
-print ("Haz de saber que tus pensamientos son los maximos deseos de tu corazón")
+print ("Haz de saber que tus pensamientos son los maximos deseos de tu corazón, que al proclamarlas constantemente y ejercer la fuerza de la voluntad sobre ellos se hacen realidad")
 print ( "¿como te llamas?")
 nombre = input()
 print ("Hello " + nombre + " ¿Cual es tu maximo deseo?")
 respuesta = input()
 print ("Crees que lo lograras")
-print ("Somos los Arcageles de la Abundancia -K, Prosperidad -GD y Riqueza -T")
+respuesta = input ()
+print (nombre + " Soy el Arcagel T encargado de darte las Riquezas que el padre de la creacion tiene para ti")
 print ("¿Te gustaria Crear o jugar?")
 respuesta = input()
-print ("Con cual de los Arcangeles te gustaria recibir tu Bendicion")
-arcangel = input()
-print ("Hello " + nombre + ", Soy el Arcangel " + arcangel + ", Haz de saber que tu maxino anhelo se hace realidad")
+print ("Hello " + nombre + ", Soy el Arcangel T "" Haz de saber que tu maxino anhelo se hace realidad")
 print (nombre + " ¿Dedeseas recibirlo?")
 respueta = input()
-print ("Arcangel " + arcangel + " dame la Bendicion que el Padre la Creación te ha dado para mí")
-
+print ("Arcangel T, dame la Bendicion que el Padre de la Creación te ha dado para mí")
 mensaje = input()
-
-print ("Concedido")
+print ("Bendicion Concedida")
 
 import random
-# l.Let's generate 6 values between 1 and 48. Store valued on a list.random
+# l.Let's generate 7 values between 0 and 48. Store valued on a list.random
 
 my_list = []
 
@@ -104,7 +102,7 @@ print(my_list)
 my_list = []
 
 for _ in range (3):
-  num = random.randrange(0, 48, 2)
+  num = random.randrange(1, 48, 2)
   my_list.append(num)
 
 print(my_list)
@@ -119,9 +117,7 @@ for _ in range(3):
 import random
 # from random impot shuffle
 
-some_nums = [i for i in range(1, 48)]
-print (some_nums)
-random.shuffle(some_nums)
-print ("Randomized: in", some_nums)
-
-
+#some_nums = [i for i in range(1, 7)]
+#print (some_nums)
+#random.shuffle(some_nums)
+#print ("Randomized: in", some_nums)
